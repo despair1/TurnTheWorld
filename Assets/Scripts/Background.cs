5ea033677c311c653d8f1a2cb87d4352bec74d09
despair1 <eveguide0@gmail.com>
2017-05-30 17:22:04 +0500

    initial

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(" main size " + Camera.main.orthographicSize);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
