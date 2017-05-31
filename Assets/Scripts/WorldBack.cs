77b0cbfdb53030cc5d068a097613bf52eb11f9ef
despair1 <eveguide0@gmail.com>
2017-05-31 12:27:38 +0500

    mouse click add ground to worldback

--- Assets/Scripts/WorldBack.cs
+++ Assets/Scripts/WorldBack.cs
@@ -19,8 +19,33 @@ public class WorldBack : MonoBehaviour {
     private void OnMouseDown()
     {
         GameObject _groundCell = Instantiate(groundCell , new Vector3(0, 0, 0), Quaternion.identity);
+        Bounds bounds = _groundCell.GetComponent<SpriteRenderer>().sprite.bounds;
         _groundCell.transform.localScale=new Vector3(bg.backgroundScale, bg.backgroundScale, 1);
+        Vector2 mousePosition = Input.mousePosition; // Event.current.mousePosition;
+        Vector3 pointOnWorldBack = Camera.main.ScreenToWorldPoint(new Vector3(
+            mousePosition.x, mousePosition.y, Camera.main.nearClipPlane));
+        /*Vector3 tilePosition = new Vector3(
+            ((int)(cellPosition[0] / bounds.size.x*2+0.5)) * bounds.size.x/2,
+            ((int)(cellPosition[1] / bounds.size.y*2+0.5)) * bounds.size.y/2,
+            cellPosition[2]);
+        */
+        _groundCell.transform.position = GetTilePosition(pointOnWorldBack, bounds); // tilePosition;
         Debug.Log("on mouse down");
 
+    }
+    private Vector3 GetTilePosition(Vector3 pointOnWorldBack, Bounds bounds)
+    {
+        return new Vector3(
+            SetSign(pointOnWorldBack[0], bounds.size.x) * bounds.size.x / 2,
+            SetSign(pointOnWorldBack[1], bounds.size.y) * bounds.size.y / 2,
+            pointOnWorldBack[2]);
+        
+    }
+    private int SetSign(float point,float bound)
+    {
+        float t = point / bound * 2;
+        if (t > 0) t += 0.5f;
+        else t -= 0.5f;
+        return (int)t;
     }
 }
